fb6d5d846768446f30cb9f312ca6a6f8e5d1d82d
nycfcas <jrosin81@yahoo.com>
2015-07-15 15:29:04 -0400

    xgb

--- ensemble.py
+++ ensemble.py
@@ -10,9 +10,15 @@ def loaddata(file):
     data = data[1:,]
     return data
 
-trainfile = '/home/jr/Documents/Kaggle/Otto Group/TrainingData/train.csv'
-testfile = '/home/jr/Documents/Kaggle/Otto Group/TestData/test.csv'
-tnsefile = '/home/jr/Documents/Kaggle/Otto Group/TrainingData/tsne.csv'
+tsnesaved = True
+xgbsaved = False
+
+thisdir = '/home/jr/Documents/Kaggle/Otto Group/TrainingData/'
+thisdir2 = '/home/jr/Documents/Kaggle/Otto Group/TestData/'
+trainfile = thisdir + 'train.csv'
+testfile = thisdir2 + 'test.csv'
+tnsefile = thisdir + 'tsne.csv'
+xgbfile = thisdir + 'xgb.csv'
 
 train = np.loadtxt(trainfile, delimiter=',', skiprows=1, converters={0: lambda x:int(x == '?'), 94: lambda x:int(x[6])-1 } )[:,1:]
 test = np.loadtxt(testfile, delimiter=',', skiprows=1, converters={0: lambda x:int(x == '?') } )[:,1:]
@@ -22,27 +28,38 @@ x = np.concatenate((train[:,:-1],test), axis = 0)
 
 x = 1/(1+np.exp(-np.sqrt(x)))
 
-#mytsne = tsne.bh_sne(x)
-#np.savetxt(tnsefile, mytsne, delimiter=',', newline='\n')
-mytnse = np.loadtxt(tnsefile, delimiter=',')
+if not tsnesaved:
+    mytsne = tsne.bh_sne(x)
+    np.savetxt(tnsefile, mytsne, delimiter=',', newline='\n')
+if tsnesaved:
+    mytnse = np.loadtxt(tnsefile, delimiter=',')
 
-np.round(x, 3)
+if not xgbsaved:
+    param = {}
+    param['objective'] = 'multi:softprob'
+    param['eval_metric'] = 'mlogloss'
+    param['num_class'] = 9
+    param['max_depth'] = 8
+    param['eta'] = .03
+    param['min_child_weight'] = 3
+    param['silent'] = 1
+    num_round = 1200
 
-param = {}
-param['objective'] = 'multi:softprob'
-param['eval_metric'] = 'mlogloss'
-param['num_class'] = 9
-param['max_depth'] = 8
-param['eta'] = .03
-param['min_child_weight'] = 3
-num_round = 2
+    np.round(x, 3)
+    x = np.append(x, mytnse, axis=1)
 
-x = np.append(x, mytnse, axis=1)
+    trainlen = y.shape[0]
 
-trainlen = y.shape[0]
+    trainX = x[:(trainlen),:]
+    testX = x[(trainlen):,:]
 
-trainX = x[:(trainlen),:]
-testX = x[(trainlen):,:]
+    xgtrain = xgb.DMatrix(trainX, label=y)
+    xgtest = xgb.DMatrix(testX)
+    watchlist = [(xgtrain, 'train')]
+    bst = xgb.train(param, xgtrain, num_round, watchlist)
 
-xgtrain = xgb.DMatrix(trainX, label=y)
-bst = xgb.train(param, xgtrain, num_round)
+    pred = bst.predict(xgtest)
+    np.savetxt(xgbfile, pred, delimiter=',', newline='\n')
+
+if xgbsaved:
+    pred = np.loadtxt(xgbfile, delimiter=',')
